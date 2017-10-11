faee8a7645be747abe1645fffd908cc9b964e902
programmiertier <zimmermannveronique@protonmail.com>
2017-10-11 10:26:48 +0200

    arrayArtikel
    Array über Artikel.
    
    Zweidimensional

--- arrayArtikel_ConsoleApplication/Program.cs
+++ arrayArtikel_ConsoleApplication/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace arrayArtikel_ConsoleApplication
 {
@@ -10,6 +7,22 @@ namespace arrayArtikel_ConsoleApplication
     {
         static void Main(string[] args)
         {
+            string[,] artikel = new string[4, 2]
+            {
+                {"Produkt 1","100" },
+                {"Produkt 2","102.2" },
+                {"Produkt 3","105.1" },
+                {"Produkt 4", "106.1" }
+            };
+            for(int produkt = 0; produkt < 4; produkt++)
+            {
+                for(int preis = 0; preis < 2; preis++)
+                {
+                    Write(artikel[produkt, preis] + "\t");
+                }
+                Write("\n");
+            }
+            ReadLine();
         }
     }
 }
